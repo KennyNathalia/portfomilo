4604421c826ec5ca6a8750dd160c31630e82f375
kennynathalia <kennynathalia@gmail.com>
2025-07-21 16:18:07 +0200

    images in seperate files with titles added

---
+++ app/data/posterData.js
@@ -0,0 +1,17 @@
+const posterData = [
+  { name: "HOUSERULESSQ.png", title: "test" },
+  { name: "KANTAPOSTER.png", title: "111110oog" },
+  { name: "LOLIQUIDATION.png", title: "111111" },
+  { name: "LorenzoLOLO.png", title: "Art 1" },
+  { name: "LOSTBOUYSHAWTY.png", title: "Art 2" },
+  { name: "NALEEPOSTER.png", title: "Art 3" },
+  { name: "poster1.jpg", title: "Art 4" },
+  { name: "poster2.jpg", title: "ASAP Amnesia" },
+  { name: "poster3.jpg", title: "Big Checks" },
+  { name: "POSTERQUINNA.png", title: "Ending" },
+  { name: "POSTERRISHAAN.png", title: "Fredom Antana" },
+  { name: "RAUWWPOSTER2.png", title: "Grrrrime" },
+  { name: "THABIGONE.png", title: "Gully vs Gaza" },
+]
+
+export default posterData;
