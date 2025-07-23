e6fe77eadccae70ae7a3a392647b2da396062f55
kennynathalia <kennynathalia@gmail.com>
2025-07-23 17:17:49 +0200

    added icons for grid an slideshow option + redirects

--- app/components/ArtPageClient.jsx
+++ app/components/ArtPageClient.jsx
@@ -5,6 +5,8 @@ import ArtSlideshow from "@components/ArtSlideshow";
 import Footer from "@components/Footer";
 import Logo from "@components/Logo";
 import Grid from "@components/GridVisual";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function ArtPageClient({ imageData, folder }) {
   const [view, setView] = useState("grid");
@@ -12,22 +14,49 @@ export default function ArtPageClient({ imageData, folder }) {
   console.log("imageData:", imageData);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <Logo width={200} height={22} />
 
       <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <button
-          onClick={() => setView("grid")}
-          className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          Grid View
-        </button>
-        <button
-          onClick={() => setView("slideshow")}
-          className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          SlideShow
-        </button>
+          <Link
+            className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/art"
+          >
+            Art
+          </Link>
+          <Link
+            className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/posters"
+          >
+            Posters
+          </Link>
+        </div>
+
+      <div className="w-full flex justify-end">
+        <div className="flex gap-4 flex-col sm:flex-row">
+          <button
+            onClick={() => setView("grid")}
+            className=""
+          >
+            <Image
+              src="/icons/grid.png"
+              width={40}
+              height={40}
+              alt="Grid View"
+            />
+          </button>
+          <button
+            onClick={() => setView("slideshow")}
+            className=""
+          >
+            <Image
+              src="/icons/slides.png"
+              width={40}
+              height={40}
+              alt="Slideshow"
+            />
+          </button>
+        </div>
       </div>
 
       <div>
--- app/components/PosterPageClient.jsx
+++ app/components/PosterPageClient.jsx
@@ -5,6 +5,8 @@ import ArtSlideshow from "./ArtSlideshow";
 import Footer from "./Footer";
 import Logo from "./Logo";
 import Grid from "./GridVisual";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function PosterPageClient({ imageData, folder }) {
   const [view, setView] = useState("grid");
@@ -14,18 +16,45 @@ export default function PosterPageClient({ imageData, folder }) {
       <Logo width={200} height={22} />
 
       <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <button
-          onClick={() => setView("grid")}
-          className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          Grid View
-        </button>
-        <button
-          onClick={() => setView("slideshow")}
-          className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        >
-          SlideShow
-        </button>
+          <Link
+            className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/art"
+          >
+            Art
+          </Link>
+          <Link
+            className="bg-yellow-500 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent hover:text-black dark:hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/posters"
+          >
+            Posters
+          </Link>
+        </div>
+
+      <div className="w-full flex justify-end">
+        <div className="flex gap-4 flex-col sm:flex-row">
+          <button
+            onClick={() => setView("grid")}
+            className=""
+          >
+            <Image
+              src="/icons/grid.png"
+              width={40}
+              height={40}
+              alt="Grid View"
+            />
+          </button>
+          <button
+            onClick={() => setView("slideshow")}
+            className=""
+          >
+            <Image
+              src="/icons/slides.png"
+              width={40}
+              height={40}
+              alt="Slideshow"
+            />
+          </button>
+        </div>
       </div>
 
       <div>
