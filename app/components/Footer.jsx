88de6f81bb8a919cafede1cd542d48b6db33903e
kennynathalia <kennynathalia@gmail.com>
2026-02-28 13:26:19 +0100

    ig link changed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       <a
         className="text-black no-underline flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://www.instagram.com/portfomilo/"
+        href="https://www.instagram.com/mudamilo/"
         target="_blank"
         rel="noopener noreferrer"
       >
